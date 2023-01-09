baaa056a7b959a5a9ec6cec1be21d393fdcb46c3
akosgarai <szlem01@gmail.com>
2023-01-09 18:55:53 +0100

    Broadcast image after selection

--- app/Http/Livewire/Component/Filepicker.php
+++ app/Http/Livewire/Component/Filepicker.php
@@ -9,10 +9,18 @@ class Filepicker extends Component
 {
     use WithFileUploads;
 
-    public $file;
+    public $file = null;
 
     public function render()
     {
         return view('livewire.component.filepicker');
+    }
+
+    public function broadcastImage()
+    {
+        $this->validate([
+            'file' => 'required|image',
+        ]);
+        $this->dispatchBrowserEvent('imageUploaded', ['fileTmpUrl' => '/storage/'.$this->file->store('tmp', 'public')]);
     }
 }
--- resources/views/livewire/component/filepicker.blade.php
+++ resources/views/livewire/component/filepicker.blade.php
@@ -1,4 +1,4 @@
 <div class="input-group mb-3">
     <input type="file" class="form-control" wire:model="file">
-    <button class="btn btn-outline-secondary" type="button" @if(!$file) disabled @endif>{{ __('Upload') }}</button>
+    <button class="btn btn-outline-secondary" type="button" @if(!$file) disabled @endif wire:click="broadcastImage">{{ __('Upload') }}</button>
 </div>
