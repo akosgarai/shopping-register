5f6ff5b058879460568bc905d0df819417977304
akosgarai <szlem01@gmail.com>
2023-01-10 17:07:16 +0100

    Parsing basics for a given receipt

--- app/Http/Livewire/Recipe/Upload.php
+++ app/Http/Livewire/Recipe/Upload.php
@@ -11,6 +11,18 @@ class Upload extends Component
     public $file;
     public $uploadedImageUrl = '';
     public $parsedText = '';
+    public $showEditor = false;
+
+    // Receipt parameters that has to be extracted from the image
+    public $receipt = [
+        'id' => '',
+        'taxNumber' => '',
+        'address' => '',
+        'name' => '',
+        'date' => '',
+        'total' => '',
+        'items' => [],
+    ];
 
     protected $listeners = ['edit.finished' => 'storeEditedImage'];
 
@@ -33,6 +45,69 @@ class Upload extends Component
         Storage::disk('public')->delete('tmp/'.$fileName);
         // Extract the text from the image
         $this->extractText($fileName);
+        $this->parseText();
+    }
+
+    public function parseText() {
+        $this->parseSparReceipt();
+        $this->showEditor = true;
+    }
+
+    private function parseSparReceipt() {
+        // Parse the receipt line by line (delimiter is end of line)
+        // Extract the receipt parameters
+        $lines = explode("\n", $this->parsedText);
+        // the first line is the name of the store
+        $this->receipt['name'] = $lines[0];
+        // the next 2 line is the address
+        $this->receipt['address'] = $lines[1].' '.$lines[2];
+        // The tax number is the 7th line. The actual tax number starts after the ':'.
+        $this->receipt['taxNumber'] = substr($lines[6], strpos($lines[6], ':')+1);
+        $firstLineAfterItems = $this->parseSparItemLines($lines, 7);
+        // The total price could be extracted from the line that starts with 'BANKKARTYA' or 'ÖSSZESEN:'
+        // The total price is the last number in the line followed by ' Ft'
+        // The id could be extracted from the line that starts with 'NYUGTASZAM:'. The id is followed by the ': '.
+        // The date could be extracted from line after the id line. The date format is 'YYYY.MM.DD, HH:MM'
+        $nextIsDate = false;
+        for ($i = $firstLineAfterItems; $i < count($lines); $i++) {
+            if ($nextIsDate) {
+                $this->receipt['date'] = substr($lines[$i], strpos($lines[$i], ': ')+2);
+                $nextIsDate = false;
+            }
+            if (strpos($lines[$i], 'BANKKARTYA') !== false || strpos($lines[$i], 'ÖSSZESEN:') !== false) {
+                $firstSpace = strpos($lines[$i], ' ');
+                $lastSpace = strrpos($lines[$i], ' ');
+                $this->receipt['total'] = substr($lines[$i], $firstSpace, $lastSpace-$firstSpace);
+            }
+            if (strpos($lines[$i], 'NYUGTASZAM:') !== false) {
+                $this->receipt['id'] = substr($lines[$i], strpos($lines[$i], ': ')+2);
+                $nextIsDate = true;
+            }
+        }
+    }
+
+    // Returns the first line index that is not item.
+    private function parseSparItemLines($lines, $from)
+    {
+        // The lines containing the items are following the next pattern:
+        // a prefix (3 character) followed by a space. The next characters are the name of the item, followed by a space and the price.
+        // Start with the 7.th line. If we found the first line that does not match the pattern, set the itemParsing flag to true.
+        // The items are finished, when the itemParsing flag is true and the next line does not match the pattern.
+        $itemParsing = false;
+        $item = [];
+        for ($i = $from; $i < count($lines); $i++) {
+            if (preg_match('/^[A-Z]{3} /', $lines[$i])) {
+                $itemParsing = true;
+                $lastSpaceIndex = strrpos($lines[$i], ' ');
+                $item['name'] = substr($lines[$i], 4, $lastSpaceIndex-4);
+                $item['price'] = substr($lines[$i], $lastSpaceIndex+1);
+                $this->receipt['items'][] = $item;
+            } else {
+                if ($itemParsing) {
+                    return $i;
+                }
+            }
+        }
     }
 
     private function extractText($fileName)
--- resources/views/livewire/recipe/upload.blade.php
+++ resources/views/livewire/recipe/upload.blade.php
@@ -11,4 +11,40 @@
             </pre>
         </div>
     @endif
+    @if ($showEditor)
+        <div class="col-12">
+            <div class="mb-3">
+                <label for="recipeName" class="form-label">{{ __('Name') }}</label>
+                <input type="text" class="form-control" wire:model="receipt.name" id="recipeName">
+            </div>
+            <div class="mb-3">
+                <label for="recipeAddress" class="form-label">{{ __('Address') }}</label>
+                <input type="text" class="form-control" wire:model="receipt.address" id="recipeAddress">
+            </div>
+            <div class="mb-3">
+                <label for="recipeTaxNumber" class="form-label">{{ __('Tax number') }}</label>
+                <input type="text" class="form-control" wire:model="receipt.taxNumber" id="recipeTaxNumber">
+            </div>
+            <!-- loop over receipt.items -->
+            @foreach ($receipt['items'] as $index => $item)
+                <div class="input-group mb-3">
+                    <input type="text" class="form-control" wire:model="receipt.items.{{ $index }}.name">
+                    <input type="text" class="form-control" wire:model="receipt.items.{{ $index }}.price">
+                    <span class="input-group-text">{{ __('Ft') }}</span>
+                </div>
+            @endforeach
+            <div class="mb-3">
+                <label for="recipeSum" class="form-label">{{ __('Total') }}</label>
+                <input type="text" class="form-control" wire:model="receipt.total" id="recipeSum">
+            </div>
+            <div class="mb-3">
+                <label for="recipeId" class="form-label">{{ __('ID') }}</label>
+                <input type="text" class="form-control" wire:model="receipt.id" id="recipeId">
+            </div>
+            <div class="mb-3">
+                <label for="recipeDate" class="form-label">{{ __('Date') }}</label>
+                <input type="text" class="form-control" wire:model="receipt.date" id="recipeDate">
+            </div>
+        </div>
+    @endif
 </div>
