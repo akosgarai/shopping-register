1124d9618d7a58a3d536b1b4969589902c037216
akosgarai <szlem01@gmail.com>
2023-01-10 14:21:22 +0100

    Update upload component

--- app/Http/Livewire/Recipe/Upload.php
+++ app/Http/Livewire/Recipe/Upload.php
@@ -3,35 +3,41 @@
 namespace App\Http\Livewire\Recipe;
 
 use Alimranahmed\LaraOCR\Services\OcrAbstract;
+use Illuminate\Support\Facades\Storage;
 use Livewire\Component;
-use Livewire\WithFileUploads;
 
 class Upload extends Component
 {
-    use WithFileUploads;
-
     public $file;
-
-    public $message = '';
-    public $parsedText = '';
     public $uploadedImageUrl = '';
+    public $parsedText = '';
+
+    protected $listeners = ['edit.finished' => 'storeEditedImage'];
 
     public function render()
     {
         return view('livewire.recipe.upload');
     }
 
-    public function save()
+    public function storeEditedImage($filepath, $imageData)
     {
-        $this->validate([
-            'file' => 'required|image',
-        ]);
-        // save file to storage/app/public/recipes
-        // save file name to database
-        $store = $this->file->store('recipes', 'public');
+        $image = $imageData;  // your base64 encoded
+        $image = str_replace('data:image/png;base64,', '', $image);
+        $image = str_replace(' ', '+', $image);
+
+        $fileName = basename($filepath);
+        $result = Storage::disk('public')->put('recipes/'.$fileName, base64_decode($image));
+        $this->uploadedImageUrl = Storage::disk('public')->url('recipes/'.$fileName);
+        // Delete the temporary file
+        $this->parsedText = $filepath;
+        Storage::disk('public')->delete('tmp/'.$fileName);
+        // Extract the text from the image
+        $this->extractText($fileName);
+    }
+
+    private function extractText($fileName)
+    {
         $ocr = app()->make(OcrAbstract::class);
-        $this->parsedText = $ocr->scan(public_path('storage/'.$store));
-        $this->uploadedImageUrl = '/storage/'.$store;
-        $this->message = 'File saved to ' . $store;
+        $this->parsedText = $ocr->scan(public_path('storage/recipes/'.$fileName));
     }
 }
--- resources/views/livewire/recipe/upload.blade.php
+++ resources/views/livewire/recipe/upload.blade.php
@@ -1,47 +1,14 @@
-<div class="card">
-    <div class="card-header">{{ __('Upload document') }}</div>
-
-    <div class="card-body">
-        @if ($message !== '')
-            <div class="alert alert-success" role="alert">
-                {{ $message }}
-            </div>
-        @endif
-        <div id="tui-image-editor" style="min-height: 800px;"></div>
-        <form wire:submit.prevent="save">
-            <input type="file" wire:model="file">
-            <div wire:loading wire:target="file">Uploading...</div>
-
-            @error('file') <span class="error">{{ $message }}</span> @enderror
-
-            <button type="submit">Upload</button>
-        </form>
-        @if ($uploadedImageUrl !== '')
+<div class="row">
+    @if ($uploadedImageUrl !== '')
+        <div class="col-6">
             <img src="{{ $uploadedImageUrl }}" alt="Uploaded image" style="max-height: 400px;">
-        @endif
-        @if ($parsedText !== '')
+        </div>
+    @endif
+    @if ($parsedText !== '')
+        <div class="col-6">
             <pre>
                     {{ $parsedText }}
             </pre>
-        @endif
-    </div>
-    <script>
-        document.addEventListener('livewire:load', function () {
-            const instance = new ImageEditor(document.querySelector('#tui-image-editor'), {
-                cssMaxWidth: 700,
-                cssMaxHeight: 1000,
-                includeUI: {
-                    menuBarPosition: 'bottom'
-                },
-                selectionStyle: {
-                    cornerSize: 20,
-                    rotatingPointOffset: 70,
-                },
-            });
-            document.addEventListener('resized', function () {
-                instance.ui.resizeEditor();
-            });
-            console.log('livewire:load', instance);
-        });
-    </script>
+        </div>
+    @endif
 </div>
