c2138440f85114250cd29bd159c4baa1d9f33187
akosgarai <szlem01@gmail.com>
2023-01-13 22:52:40 +0100

    Remove unnecessary use statement

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -5,8 +5,6 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-use App\Models\Address;
-
 class Company extends Model
 {
     use HasFactory;
