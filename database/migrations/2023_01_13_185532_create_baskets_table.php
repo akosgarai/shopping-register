a50a49a81f6f7d8d8b91f6093470f386e36d9d5d
akosgarai <szlem01@gmail.com>
2023-01-13 21:00:36 +0100

    Migration script for the baskets table

---
+++ database/migrations/2023_01_13_185532_create_baskets_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('baskets', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->foreignId('shop_id')->constrained();
+            $table->foreignId('user_id')->constrained();
+            $table->dateTime('date');
+            $table->decimal('total', 8, 2);
+            $table->string('receipt_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('baskets');
+    }
+};
