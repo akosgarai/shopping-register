1454cda844cfb3387019f985977dcc8f40eb1ed2
akosgarai <szlem01@gmail.com>
2023-01-16 15:09:31 +0100

    Add new route to the receipt list page

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,4 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/scan', [App\Http\Controllers\ScanController::class, 'index'])->name('scan');
+Route::get('/receipts', [App\Http\Controllers\ReceiptController::class, 'index'])->name('receipts');
