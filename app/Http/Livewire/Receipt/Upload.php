099c51c6a27dcea34a76629bbaad025633b92d5a
akosgarai <szlem01@gmail.com>
2023-01-16 14:08:42 +0100

    Save the extracted and fixed data

--- app/Http/Livewire/Receipt/Upload.php
+++ app/Http/Livewire/Receipt/Upload.php
@@ -6,6 +6,12 @@ use Alimranahmed\LaraOCR\Services\OcrAbstract;
 use Illuminate\Support\Facades\Storage;
 use Livewire\Component;
 
+use App\Models\Address;
+use App\Models\Basket;
+use App\Models\BasketItem;
+use App\Models\Company;
+use App\Models\Item;
+use App\Models\Shop;
 use App\Services\Parser\SparParserService;
 
 class Upload extends Component
@@ -30,6 +36,18 @@ class Upload extends Component
 
     protected $listeners = ['edit.finished' => 'storeEditedImage'];
 
+    protected $rules = [
+        'receipt.id' => 'required',
+        'receipt.taxNumber' => 'required',
+        'receipt.marketAddress' => 'required',
+        'receipt.marketName' => 'required',
+        'receipt.companyAddress' => 'required',
+        'receipt.companyName' => 'required',
+        'receipt.date' => 'required',
+        'receipt.total' => 'required',
+        'receipt.items' => 'required',
+    ];
+
     public function render()
     {
         return view('livewire.receipt.upload');
@@ -52,9 +70,56 @@ class Upload extends Component
         $this->parseText();
     }
 
-    public function parseText() {
+    public function parseText()
+    {
         $this->receipt = (new SparParserService($this->parsedText))->parse();
         $this->showEditor = true;
+    }
+
+    public function submit()
+    {
+        $this->validate();
+
+        // First handle the addresses to get the ids
+        $companyAddress = Address::firstOrCreate([
+            'raw' => $this->receipt['companyAddress'],
+        ]);
+        $marketAddress = Address::firstOrCreate([
+            'raw' => $this->receipt['marketAddress'],
+        ]);
+        // Then handle the company and then the shop
+        $company = Company::firstOrCreate([
+            'name' => $this->receipt['companyName'],
+            'tax_number' => $this->receipt['taxNumber'],
+            'address_id' => $companyAddress->id,
+        ]);
+        $shop = Shop::firstOrCreate([
+            'name' => $this->receipt['marketName'],
+            'address_id' => $marketAddress->id,
+            'company_id' => $company->id,
+        ]);
+        // Then create the basket
+        $basket = Basket::create([
+            'shop_id' => $shop->id,
+            'date' => $this->receipt['date'],
+            'total' => $this->receipt['total'],
+            'receipt_id' => $this->receipt['id'],
+            'user_id' => auth()->user()->id,
+        ]);
+        // If we have items, handle them
+        // then create the basket items.
+        if (count($this->receipt['items']) > 0) {
+            foreach ($this->receipt['items'] as $item) {
+                $product = Item::firstOrCreate([
+                    'name' => $item['name'],
+                ]);
+                BasketItem::create([
+                    'basket_id' => $basket->id,
+                    'item_id' => $product->id,
+                    'price' => $item['price'],
+                ]);
+            }
+        }
     }
 
     private function extractText($fileName)
@@ -63,7 +128,8 @@ class Upload extends Component
         $this->parsedText = $ocr->scan(public_path('storage/receipts/'.$fileName));
     }
 
-    private function initReceipt() {
+    private function initReceipt()
+    {
         $this->receipt = [
             'id' => '',
             'taxNumber' => '',
--- resources/views/livewire/receipt/upload.blade.php
+++ resources/views/livewire/receipt/upload.blade.php
@@ -12,26 +12,32 @@
         </div>
     @endif
     @if ($showEditor)
+    <form wire:submit.prevent="submit">
         <div class="col-12">
             <div class="mb-3">
                 <label for="receiptName" class="form-label">{{ __('Company name') }}</label>
                 <input type="text" class="form-control" wire:model="receipt.companyName" id="receiptName">
+                @error('receipt.companyName') <span class="error">{{ $message }}</span> @enderror
             </div>
             <div class="mb-3">
                 <label for="receiptAddress" class="form-label">{{ __('Company address') }}</label>
                 <input type="text" class="form-control" wire:model="receipt.companyAddress" id="receiptAddress">
+                @error('receipt.companyAddress') <span class="error">{{ $message }}</span> @enderror
             </div>
             <div class="mb-3">
                 <label for="receiptTaxNumber" class="form-label">{{ __('Tax number') }}</label>
                 <input type="text" class="form-control" wire:model="receipt.taxNumber" id="receiptTaxNumber">
+                @error('receipt.taxNumber') <span class="error">{{ $message }}</span> @enderror
             </div>
             <div class="mb-3">
                 <label for="receiptName" class="form-label">{{ __('Market name') }}</label>
                 <input type="text" class="form-control" wire:model="receipt.marketName" id="receiptName">
+                @error('receipt.marketName') <span class="error">{{ $message }}</span> @enderror
             </div>
             <div class="mb-3">
                 <label for="receiptAddress" class="form-label">{{ __('Market address') }}</label>
                 <input type="text" class="form-control" wire:model="receipt.marketAddress" id="receiptAddress">
+                @error('receipt.marketAddress') <span class="error">{{ $message }}</span> @enderror
             </div>
             <!-- loop over receipt.items -->
             @foreach ($receipt['items'] as $index => $item)
@@ -39,20 +45,28 @@
                     <input type="text" class="form-control" wire:model="receipt.items.{{ $index }}.name">
                     <input type="text" class="form-control" wire:model="receipt.items.{{ $index }}.price">
                     <span class="input-group-text">{{ __('Ft') }}</span>
+                @error('receipt.items.{{ $index }}.price') <span class="error">{{ $message }}</span> @enderror
                 </div>
             @endforeach
             <div class="mb-3">
                 <label for="receiptSum" class="form-label">{{ __('Total') }}</label>
                 <input type="text" class="form-control" wire:model="receipt.total" id="receiptSum">
+                @error('receipt.total') <span class="error">{{ $message }}</span> @enderror
             </div>
             <div class="mb-3">
                 <label for="receiptId" class="form-label">{{ __('ID') }}</label>
                 <input type="text" class="form-control" wire:model="receipt.id" id="receiptId">
+                @error('receipt.id') <span class="error">{{ $message }}</span> @enderror
             </div>
             <div class="mb-3">
                 <label for="receiptDate" class="form-label">{{ __('Date') }}</label>
                 <input type="text" class="form-control" wire:model="receipt.date" id="receiptDate">
+                @error('receipt.date') <span class="error">{{ $message }}</span> @enderror
+            </div>
+            <div class="mb-3">
+                <button type="submit">{{ __('Save') }}</button>
             </div>
         </div>
+    </form>
     @endif
 </div>
