35eca8a7cfcd6d1c386a5e1e9ae3d3275cfcc400
akosgarai <szlem01@gmail.com>
2023-01-17 13:20:58 +0100

    Display the receipt id instead of the db id on the card

--- resources/views/livewire/receipt/card.blade.php
+++ resources/views/livewire/receipt/card.blade.php
@@ -1,7 +1,7 @@
 <div class="card">
     <div class="card-body">
         <h4 class="card-title">{{ $basket->shop->name }} <span class="badge bg-light">
-                <a href="{{ URL::route('receipts.view', [ 'id' => $basket->id ]) }}" target="blank">{{ $basket->id }}</a>
+                <a href="{{ URL::route('receipts.view', [ 'id' => $basket->id ]) }}" target="blank">{{ $basket->receipt_id }}</a>
             </span>
         </h4>
         <h5 class="card-subtitle">{{ $basket->date }}</h5>
