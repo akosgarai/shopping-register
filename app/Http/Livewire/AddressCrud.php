cf7f3d710db762d50b7a1ad3552338b16b931529
akosgarai <szlem01@gmail.com>
2023-01-18 13:49:14 +0100

    Edit form basics - offcanvas with query param based routing

--- app/Http/Livewire/AddressCrud.php
+++ app/Http/Livewire/AddressCrud.php
@@ -8,9 +8,20 @@ use App\Models\Address;
 
 class AddressCrud extends Component
 {
+    public $action = '';
+
+    protected $queryString = [
+        'action' => ['except' => ''],
+    ];
+
     public function render()
     {
         return view('livewire.address-crud', [ 'addresses' =>  Address::all() ])
             ->extends('layouts.app');
+    }
+
+    public function setAction($action)
+    {
+        $this->action = $action;
     }
 }
--- resources/views/livewire/address-crud.blade.php
+++ resources/views/livewire/address-crud.blade.php
@@ -1,4 +1,5 @@
 <div class="container">
+    <button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#newAddress" aria-controls="newAddress" wire:click="setAction('new')">{{ __('New Address') }}</button>
     <table class="table table-striped table-hover">
         <thead>
             <tr class="table-dark">
@@ -15,4 +16,18 @@
             @endforeach
         </tbody>
     </table>
+    <div wire:ignore>
+        <div class="offcanvas offcanvas-start @if($action == 'new') show @endif" data-bs-scroll="true" tabindex="-1" id="newAddress" aria-labelledby="newAddressLabel">
+            <div class="offcanvas-header">
+                <h5 class="offcanvas-title" id="newAddressLabel">{{ __('New Address') }}</h5>
+                <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close" wire:click="setAction('')"></button>
+            </div>
+            <div class="offcanvas-body">
+                <div class="mb-3">
+                    <label for="addressRaw" class="form-label">{{ __('Address') }}</label>
+                    <input type="text" class="form-control" id="addressRaw">
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
