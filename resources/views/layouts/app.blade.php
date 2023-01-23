76219758bddcf3c0cf45dc5aa1f658580ce0a0bf
akosgarai <szlem01@gmail.com>
2023-01-23 15:41:33 +0100

    Add baskets to menu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -60,6 +60,7 @@
                                     <a class="dropdown-item" href="{{ route('item') }}">{{ __('Items') }}</a>
                                     <a class="dropdown-item" href="{{ route('company') }}">{{ __('Companies') }}</a>
                                     <a class="dropdown-item" href="{{ route('shop') }}">{{ __('Shops') }}</a>
+                                    <a class="dropdown-item" href="{{ route('basket') }}">{{ __('Baskets') }}</a>
 
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
