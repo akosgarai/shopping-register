d40edb1e6afeff76291ccdefc4a1778b16ff0d82
akosgarai <szlem01@gmail.com>
2023-01-30 18:22:07 +0100

    Improve image upload process

--- app/Http/Livewire/ReceiptScan.php
+++ app/Http/Livewire/ReceiptScan.php
@@ -2,20 +2,29 @@
 
 namespace App\Http\Livewire;
 
+use Illuminate\Validation\ValidationException;
 use Livewire\Component;
+use Livewire\WithFileUploads;
+
+use App\Services\ImageService;
 
 class ReceiptScan extends Component
 {
+    use WithFileUploads;
+
     const ACTION_PICK = 'pick';
     const ACTION_EDIT = 'edit';
     const ACTION_PARSE = 'parse';
     const ACTION_BASKET = 'basket';
 
     public $action = '';
+    public $tempImage = null;
+    public $imagePath = '';
 
     // The query string parameters.
     protected $queryString = [
         'action' => ['except' => ''],
+        'imagePath' => ['except' => '', 'as' => 'document'],
     ];
 
     // Initialize the component based on the query string parameters.
@@ -35,6 +44,24 @@ class ReceiptScan extends Component
     public function offcanvasOpen()
     {
         $this->action = self::ACTION_PICK;
+        $this->imagePath = '';
         $this->dispatchBrowserEvent('receiptScan.pick');
+    }
+
+    public function saveTempImage(ImageService $imageService)
+    {
+        try {
+            $this->validate([
+                'tempImage' => 'required|image',
+            ]);
+        } catch (ValidationException $e) {
+            $messages = $e->validator->getMessageBag();
+            $this->dispatchBrowserEvent('model.validation', ['type' => 'pick', 'model' => 'ImageOffcanvas', 'messages' => $messages]);
+            return;
+        }
+        $receiptUrl = $imageService->saveTempImageToUserFolder($this->tempImage, auth()->user()->id);
+        $this->imagePath = basename($receiptUrl);
+        $this->action = self::ACTION_EDIT;
+        $this->dispatchBrowserEvent('receiptScan.edit');
     }
 }
--- resources/views/livewire/receipt-scan.blade.php
+++ resources/views/livewire/receipt-scan.blade.php
@@ -28,17 +28,42 @@
                 <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close" wire:click='$set("action", "")'></button>
             </div>
             <div class="offcanvas-body">
-                Content goes here.
+                <form wire:submit.prevent="saveTempImage">
+                    <div class="mb-3">
+                        <label for="tempImage" class="form-label">{{ __('Upload Image') }}</label>
+                        <input type="file" class="form-control" id="tempImage" wire:model="tempImage"
+                            onclick="hideImageUploadSubmitButton()"
+                            onchange="validateImage(this.value);">
+                        <span id="errors-tempImage" class="text-danger" style="display: none;"></span>
+                    </div>
+                    <button type="submit" class="btn btn-primary" id="uploadTempImageButton" @if($tempImage == '') style="display:none;" @endif>{{ __('Save') }}</button>
+                </form>
             </div>
         </div>
+        <script>
+            function hideImageUploadSubmitButton() {
+                document.querySelector('#uploadTempImageButton').style.display = 'none';
+            }
+            function validateImage(path) {
+                if (path !== '') {
+                    document.querySelector('#uploadTempImageButton').style.display = 'block';
+                }
+            }
+        </script>
     </div>
     @if($action == self::ACTION_PICK)
         <script>
             window.addEventListener('livewire:load', function () {
                 var offcanvas = new bootstrap.Offcanvas(document.getElementById('pickImageOffcanvas'));
                 offcanvas.show();
+                hideImageUploadSubmitButton();
             });
         </script>
+    @endif
+    @if($action == self::ACTION_EDIT)
+        @if ($imagePath)
+            <img src="{{ route('image.viewTemp', ['filename' => $imagePath]) }}" alt="tempImage" class="img-fluid" style="width: 100%;">
+        @endif
     @endif
     @include('livewire.component.offcanvasscipts')
 </div>
