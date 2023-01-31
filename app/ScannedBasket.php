f6c7d7999db93554d37cf490ba93b0d84b1db118
akosgarai <szlem01@gmail.com>
2023-01-31 14:26:01 +0100

    Distinct class for the scanned basket item

---
+++ app/ScannedBasket.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+class ScannedBasket
+{
+    public $id = '';
+    public $taxNumber = '';
+    public $marketAddress = '';
+    public $marketName = '';
+    public $companyAddress = '';
+    public $companyName = '';
+    public $date = '';
+    public $total = '';
+    public $items = [];
+}
