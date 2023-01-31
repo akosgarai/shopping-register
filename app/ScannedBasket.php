71601416763e156a851f88f8f84d1df757004be0
akosgarai <szlem01@gmail.com>
2023-01-31 19:29:00 +0100

    toArray method to make it js friendly data type

--- app/ScannedBasket.php
+++ app/ScannedBasket.php
@@ -13,4 +13,19 @@ class ScannedBasket
     public $date = '';
     public $total = '';
     public $items = [];
+
+    public function toArray(): array
+    {
+        return [
+            'id' => $this->id,
+            'taxNumber' => $this->taxNumber,
+            'marketAddress' => $this->marketAddress,
+            'marketName' => $this->marketName,
+            'companyAddress' => $this->companyAddress,
+            'companyName' => $this->companyName,
+            'date' => $this->date,
+            'total' => $this->total,
+            'items' => $this->items,
+        ];
+    }
 }
