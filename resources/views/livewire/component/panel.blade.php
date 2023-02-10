340e1959943492457e29351485af7edb80013910
akosgarai <szlem01@gmail.com>
2023-02-10 20:31:22 +0100

    Panel close means step back

--- resources/views/livewire/component/panel.blade.php
+++ resources/views/livewire/component/panel.blade.php
@@ -2,7 +2,7 @@
     <div class="panel {{ $position }} @if($open) visible @endif" aria-labelledby="{{ $panelName }}Label">
         <div class="panel-header">
             <h3 class="panel-title" id="{{ $panelName }}Label">{{ $panelTitle }}</h3>
-            <button type="button" class="btn-close text-reset"  aria-label="{{ __('Close') }}" wire:click="$emitSelf('panel.close');"></button>
+            <button type="button" class="btn-close text-reset"  aria-label="{{ __('Close') }}" wire:click="$emitUp('action.back');"></button>
         </div>
         <div class="panel-body">
             @include($contentTemplate, $contentParameters)
