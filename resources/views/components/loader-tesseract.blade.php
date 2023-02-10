62c3c72756bc36b033636b9454403159d8661207
akosgarai <szlem01@gmail.com>
2023-02-10 14:08:40 +0100

    Loading screen

---
+++ resources/views/components/loader-tesseract.blade.php
@@ -0,0 +1,21 @@
+<div class="tesseract">
+    <div class="shape">
+        <div class="cube outer">
+            <div class="side left"></div>
+            <div class="side right"></div>
+            <div class="side top"></div>
+            <div class="side bottom"></div>
+            <div class="side front"></div>
+            <div class="side back"></div>
+
+            <div class="cube">
+                <div class="side left"></div>
+                <div class="side right"></div>
+                <div class="side top"></div>
+                <div class="side bottom"></div>
+                <div class="side front"></div>
+                <div class="side back"></div>
+            </div>
+        </div>
+    </div>
+</div>
