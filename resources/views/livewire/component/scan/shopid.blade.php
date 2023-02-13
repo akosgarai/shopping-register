6a13a8370565644fb72f82494b816a59d120d82e
akosgarai <szlem01@gmail.com>
2023-02-13 14:14:05 +0100

    Fix - variable name

--- resources/views/livewire/component/scan/shopid.blade.php
+++ resources/views/livewire/component/scan/shopid.blade.php
@@ -2,6 +2,6 @@
     <livewire:component.scan.shop
         :scannedName="array_key_exists('marketName', $basket) ? $basket['marketName'] : ''"
         :scannedAddress="array_key_exists('marketAddress', $basket) ? $basket['marketAddress'] : ''"
-        :shopCompany="array_key_exists('company_id', $basket) ? $basket['company_id'] : ''"
+        :shopCompany="array_key_exists('companyId', $basket) ? $basket['companyId'] : ''"
     >
 </div>
