99d25fc32126099d320d0695749313ee6bbf4a4f
akosgarai <szlem01@gmail.com>
2023-02-13 19:35:49 +0100

    Basket item form basics

--- app/Http/Livewire/Component/Scan/BasketItem.php
+++ app/Http/Livewire/Component/Scan/BasketItem.php
@@ -4,6 +4,7 @@ namespace App\Http\Livewire\Component\Scan;
 
 use Livewire\Component;
 
+use App\Models\Item;
 use App\Services\DataPredictionService;
 
 class BasketItem extends Component
@@ -14,7 +15,7 @@ class BasketItem extends Component
 
     // The edited values.
     public $total = '';
-    public $items = '';
+    public $items = [];
 
     // This is the value set by the Shop component.
     public $shopId = '';
@@ -27,6 +28,12 @@ class BasketItem extends Component
     {
         $this->total = $this->scannedTotal;
         $this->items = $this->scannedItems;
+        // extend the items with the itemId key if not already set.
+        foreach ($this->items as $key => $item) {
+            if (!isset($item['itemId'])) {
+                $this->items[$key]['itemId'] = '';
+            }
+        }
     }
 
     public function render()
@@ -42,7 +49,36 @@ class BasketItem extends Component
         $this->mount();
         $this->getPredictions($dataPrediction);
     }
+
+    public function insertNew($index, DataPredictionService $dataPrediction)
+    {
+        $item = Item::firstOrCreate([
+            'name' => $this->items[$index]['name'],
+        ]);
+        $this->items[$index]['itemId'] = $item->id;
+        $this->emitUp('basket.data.update', ['items' => $this->items, 'total' => $this->total]);
+        $this->getPredictions($dataPrediction);
+    }
+
+    public function deleteItem($index)
+    {
+        unset($this->items[$index]);
+        $this->emitUp('basket.data.update', ['items' => $this->items, 'total' => $this->total]);
+    }
+
+    public function finishedEdition()
+    {
+        $this->emitUp('basket.data.done');
+    }
+
     private function getPredictions(DataPredictionService $dataPrediction)
     {
+        foreach ($this->items as $key => $item) {
+            $this->items[$key]['suggestions'] = $dataPrediction->getItemSuggestions($item['name']);
+            // if the current selection is not set and the first suggestion has 0 distance, set it.
+            if ($item['itemId'] == '' && count($this->items[$key]['suggestions']) > 0 && $this->items[$key]['suggestions'][0]['distance'] == 0) {
+                $this->items[$key]['itemId'] = $this->items[$key]['suggestions'][0]['id'];
+            }
+        }
     }
 }
--- resources/views/livewire/component/scan/basket-item.blade.php
+++ resources/views/livewire/component/scan/basket-item.blade.php
@@ -3,5 +3,29 @@
         <h3>{{ __('You have to setup the Shop first.') }}</h3>
     @else
         <h3>{{ __('You have Shop. Very nice.') }} - {{ $scannedTotal }}</h3>
+        <hr>
+        @foreach ($items as $index => $item)
+            @if ($item['itemId'] == '')
+                <div class="input-group mb-3">
+                    <input type="text" class="form-control" wire:model="items.{{ $index }}.name">
+                    <input class="btn btn-outline-secondary" type="button" value="{{ __('Add new') }}" wire:click="insertNew({{ $index }})">
+                </div>
+            @endif
+            <div class="input-group mb-3">
+                <select class="form-select" id="selectedAddress-{{ $index }}" wire:model="items.{{ $index }}.itemId">
+                    <option value="" @if($item['itemId'] == '') selected @endif>{{ __('Scanned text') }}</option>
+                    @foreach ($item['suggestions'] as $itemSuggestion)
+                        <option value="{{ $itemSuggestion['id'] }}"
+                            @if($item['itemId'] == $itemSuggestion['id']) selected @endif
+                            >{{ $itemSuggestion['name'] }} ({{ $itemSuggestion['percentage'] }}%)</option>
+                    @endforeach
+                </select>
+                <input type="text" class="form-control" wire:model="items.{{ $index }}.price">
+                <span class="input-group-text">{{ __('Ft') }}</span>
+                <button class="btn btn-outline-secondary" type="button" wire:click="deleteItem({{ $index }})"><i class="bi bi-trash-fill"></i></button>
+            </div>
+            @error('items.{{ $index }}.price') <span class="error">{{ $message }}</span> @enderror
+            <hr>
+        @endforeach
     @endif
 </div>
