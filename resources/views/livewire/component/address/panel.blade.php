2f08697327a3530278472c53f01ab4e7200aac52
akosgarai <szlem01@gmail.com>
2023-02-21 18:34:17 +0100

    Readonly param config for address

--- resources/views/livewire/component/address/panel.blade.php
+++ resources/views/livewire/component/address/panel.blade.php
@@ -2,7 +2,9 @@
     <livewire:component.crud-action
         :modelName="'address'" :action="$contentParameters['action']"
         :formData="[
-            ['keyName' => 'raw', 'type' => 'textinput', 'label' => __('Address'), 'rules' => 'required|string|max:255'],
+            ['keyName' => 'raw', 'type' => 'textinput', 'label' => __('Address'), 'rules' => 'required|string|max:255', 'readonly' => false],
+            ['keyName' => 'createdAt', 'type' => 'textinput', 'label' => __('Created'), 'rules' => '', 'readonly' => true],
+            ['keyName' => 'updatedAt', 'type' => 'textinput', 'label' => __('Updated'), 'rules' => '', 'readonly' => true],
         ]"
         :modelData="$contentParameters['address']"
         :wire:key="'address-panel'" />
