4df8a0f0043d5f57a2729d39bc8703e4b274a233
akosgarai <szlem01@gmail.com>
2023-02-21 18:33:48 +0100

    Display readonly form data if necessary

--- resources/views/livewire/component/crud-action.blade.php
+++ resources/views/livewire/component/crud-action.blade.php
@@ -5,7 +5,7 @@
             <div class="card-body">
                 <form wire:submit.prevent="create">
                     @foreach($formData as $key => $formElement)
-                        @if($formElement['type'] == 'textinput')
+                        @if($formElement['type'] == 'textinput' && $formElement['readonly'] == false)
                             @include('livewire.component.offcanvasform.textinput', ['modelId' => 'modelData.' . $formElement['keyName'], 'formLabel' => $formElement['label']])
                         @endif
                     @endforeach
@@ -20,7 +20,7 @@
             <h4 class="card-header"><i class="bi bi-eyeglasses me-3"></i>{{ __('View') }}</h4>
             <div class="card-body">
                 @foreach($formData as $key => $formElement)
-                    @if($formElement['type'] == 'textinput')
+                    @if($formElement['type'] == 'textinput' && $formElement['readonly'] == false)
                         <p class="card-text fw-bold ms-2 fs-3">{{ $modelData[$formElement['keyName']] }}</p>
                     @endif
                 @endforeach
@@ -33,7 +33,7 @@
                 <form wire:submit.prevent="update">
                     @foreach($formData as $key => $formElement)
                         @if($formElement['type'] == 'textinput')
-                            @include('livewire.component.offcanvasform.textinput', ['modelId' => 'modelData.' . $formElement['keyName'], 'formLabel' => $formElement['label']])
+                            @include('livewire.component.offcanvasform.textinput', ['modelId' => 'modelData.' . $formElement['keyName'], 'formLabel' => $formElement['label'], 'readonly' => $formElement['readonly']])
                         @endif
                     @endforeach
                     <div class="d-flex flex-row-reverse">
@@ -48,7 +48,7 @@
             <div class="card-body">
                 <h5 class="card-title mb-2">{{ __('Are you sure you want to delete?') }}</h5>
                 @foreach($formData as $key => $formElement)
-                    @if($formElement['type'] == 'textinput')
+                    @if($formElement['type'] == 'textinput' && $formElement['readonly'] == false)
                         <p class="card-text fw-bold ms-2 fs-3">{{ $modelData[$formElement['keyName']] }}</p>
                     @endif
                 @endforeach
