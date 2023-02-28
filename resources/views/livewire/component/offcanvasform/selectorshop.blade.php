06c828dc96a40511f515d8bfb0450c2bc3f3f118
akosgarai <szlem01@gmail.com>
2023-02-28 15:20:39 +0100

    Shop selector array access

--- resources/views/livewire/component/offcanvasform/selectorshop.blade.php
+++ resources/views/livewire/component/offcanvasform/selectorshop.blade.php
@@ -3,8 +3,8 @@
     <select class="form-select" wire:model="{{ $modelId }}" id="{{ $modelId }}">
         <option value="" @if($selected == "") selected @endif>{{ __('Select Shop') }}</option>
         @foreach ($shops as $shop)
-            <option value="{{ $shop['id'] }}" @if($selected == $shop['id']) selected @endif>{{ $shop->name }}, {{ $shop->address->raw }}</option>
+            <option value="{{ $shop['id'] }}" @if($selected == $shop['id']) selected @endif>{{ $shop['name'] }}, {{ $shop['address']['raw'] }}</option>
         @endforeach
     </select>
-    <span id="errors-{{ $modelId }}" class="text-danger" style="display: none;"></span>
+    <span id="errors-{{ $modelId }}" class="text-danger" >@error($modelId) {{ $message }} @enderror</span>
 </div>
