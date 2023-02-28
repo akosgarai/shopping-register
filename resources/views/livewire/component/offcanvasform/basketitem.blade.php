7e12d0f41dc56d3a51db5789c8e1e728e42ea5c3
akosgarai <szlem01@gmail.com>
2023-02-28 18:09:05 +0100

    Fix basket item template

--- resources/views/livewire/component/offcanvasform/basketitem.blade.php
+++ resources/views/livewire/component/offcanvasform/basketitem.blade.php
@@ -1,9 +1,9 @@
-<div class="mb-3" id="basketItemTemplate-{{ $templateIndex }}">
+<div class="mb-3" id="basketItemTemplate-{{ $templateIndex }}" wire:key="basketItemTemplate-{{ $templateIndex }}">
     <div class="input-group">
         <select class="form-select" wire:model="{{ $itemModelId }}" id="{{ $itemModelId }}">
             <option value="" @if($selected == "") selected @endif>{{ __('Select Item') }}</option>
-            @foreach ($items as $item)
-                <option value="{{ $item['id'] }}" @if($selected == $item['id']) selected @endif>{{ $item->name }}</option>
+            @foreach ($options as $item)
+                <option value="{{ $item['id'] }}" @if($selected == $item['id']) selected @endif>{{ $item['name'] }}</option>
             @endforeach
         </select>
         <input type="number" class="form-control" id="{{ $priceModelId }}" step="0.01" wire:model="{{ $priceModelId }}">
