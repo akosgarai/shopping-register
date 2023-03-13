b01d19cb5d5887468547625f9978dca3b66bc6d3
akosgarai <szlem01@gmail.com>
2023-03-13 16:30:41 +0100

    Refactor and fixes based on the sample data

--- app/Services/AbstractParserService.php
+++ app/Services/AbstractParserService.php
@@ -6,5 +6,36 @@ use App\ScannedBasket;
 
 abstract class AbstractParserService
 {
+    public $rawText = '';
+
+    public $lines = [];
+
+    // Receipt parameters that has to be extracted from the text.
+    private ScannedBasket $receipt;
+
     abstract public function parse(string $rawText): ScannedBasket;
+
+    // Return the line index of the line that has the lowest levenshtein distance to the given pattern.
+    // If the pattern is not found, return -1.
+    protected function getLineIndexWithLowestLevenshteinDistance($pattern, $startIndex = 0)
+    {
+        $minDistance = 999999;
+        $minDistanceIndex = -1;
+        $numberOfLines = count($this->lines);
+        for ($i = $startIndex; $i < $numberOfLines ; $i++) {
+            // Check only the first $pattern characters of the line.
+            $distance = levenshtein(substr($this->lines[$i], 0, strlen($pattern)), $pattern);
+            if ($distance < $minDistance) {
+                $minDistance = $distance;
+                $minDistanceIndex = $i;
+            }
+        }
+        return $minDistanceIndex;
+    }
+
+    // Explode the text into lines.
+    protected function explodeTextIntoLines($delimiter = PHP_EOL)
+    {
+        $this->lines = explode($delimiter, $this->rawText);
+    }
 }
--- app/Services/Parser/SparParserService.php
+++ app/Services/Parser/SparParserService.php
@@ -13,9 +13,7 @@ class SparParserService extends AbstractParserService
     public const CREDIT_CARD_PATTERN = 'BANKKARTYA';
     public const ID_PATTERN = 'NYUGTASZAM';
 
-    public $rawText = '';
-    // Receipt parameters that has to be extracted from the text.
-    private ScannedBasket $receipt;
+    private $readLineIndex = 0;
 
     public function parse(string $rawText): ScannedBasket
     {
@@ -26,51 +24,41 @@ class SparParserService extends AbstractParserService
     }
 
     private function parseSparReceipt() {
-        // Parse the receipt line by line (delimiter is end of line)
-        // Extract the receipt parameters
-        $lines = explode("\n", $this->rawText);
-        // the first line is the name of the store
-        $this->receipt->companyName = trim($lines[0]);
-        // Everything before the line that starts with the MARKET_NAME_PATTERN is the address
-        $companyAddress = '';
-        $marketNameLine = $this->getLineIndexWithLowestLevenshteinDistance($lines, self::MARKET_NAME_PATTERN);
-        $this->receipt->marketName = trim($lines[$marketNameLine]);
-        for ($index = 1; $index < $marketNameLine; $index++) {
-            $companyAddress .= $lines[$index] . ' ';
-        }
-        $this->receipt->companyAddress = trim($companyAddress);
-        $taxNumberLine = $this->getLineIndexWithLowestLevenshteinDistance($lines, self::TAX_NUMBER_PATTERN);
-        $this->receipt->taxNumber = trim(substr($lines[$taxNumberLine], strpos($lines[$taxNumberLine], ' ')+1));
-        // The market address are in the lines between the MARKET_NAME_PATTERN and the TAX_NUMBER_PATTERN
-        $marketAddress = '';
-        for ($index = $marketNameLine + 1; $index < $taxNumberLine; $index++) {
-            $marketAddress .= $lines[$index] . ' ';
-        }
-        $this->receipt->marketAddress = trim($marketAddress);
-        $firstLineAfterItems = $this->parseSparItemLines($lines, 7);
+        $this->explodeTextIntoLines("\n");
+        // the first line is the name of the company.
+        $this->setupCompanyName();
+        // The next lines are the address of the company.
+        $this->setupCompanyAddress();
+        // The next line holds the market name.
+        $this->setupMarketName();
+        // The next lines before the tax number are the address of the market.
+        $this->setupMarketAddress($this->getLineIndexWithLowestLevenshteinDistance(self::TAX_NUMBER_PATTERN, $this->readLineIndex));
+        // The tax number is followed by the 'ADÓSZÁM' pattern.
+        $this->setupTaxNumber();
+        $firstLineAfterItems = $this->parseSparItemLines($this->lines, $this->readLineIndex);
         // The total price could be extracted from the line that starts with 'BANKKARTYA' or 'ÖSSZESEN:'
-        $totalLineIndex = $this->getLineIndexWithLowestLevenshteinDistance($lines, self::TOTAL_PATTERN, $firstLineAfterItems);
-        $creditCardLineIndex = $this->getLineIndexWithLowestLevenshteinDistance($lines, self::CREDIT_CARD_PATTERN, $firstLineAfterItems);
+        $totalLineIndex = $this->getLineIndexWithLowestLevenshteinDistance(self::TOTAL_PATTERN, $firstLineAfterItems);
+        $creditCardLineIndex = $this->getLineIndexWithLowestLevenshteinDistance(self::CREDIT_CARD_PATTERN, $firstLineAfterItems);
         // Check Total line. If the text similarity is higher than 0.8, then it is the total line.
         $totalLineSimilarityPercent = 0;
-        similar_text(self::TOTAL_PATTERN, substr($lines[$totalLineIndex], 0, 8), $totalLineSimilarityPercent);
+        similar_text(self::TOTAL_PATTERN, substr($this->lines[$totalLineIndex], 0, 8), $totalLineSimilarityPercent);
         $creditCardLineSimilarityPercent = 0;
-        similar_text(self::CREDIT_CARD_PATTERN, substr($lines[$creditCardLineIndex], 0, 10), $creditCardLineSimilarityPercent);
+        similar_text(self::CREDIT_CARD_PATTERN, substr($this->lines[$creditCardLineIndex], 0, 10), $creditCardLineSimilarityPercent);
         // The total price is the last number in the line followed by ' Ft'
         if ($totalLineSimilarityPercent > 80) {
-            $firstSpace = strpos($lines[$totalLineIndex], ' ');
-            $lastSpace = strrpos($lines[$totalLineIndex], ' ');
-            $this->receipt->total = substr($lines[$totalLineIndex], $firstSpace, $lastSpace-$firstSpace);
+            $firstSpace = strpos($this->lines[$totalLineIndex], ' ');
+            $lastSpace = strrpos($this->lines[$totalLineIndex], ' ');
+            $this->receipt->total = substr($this->lines[$totalLineIndex], $firstSpace, $lastSpace-$firstSpace);
         } else if ($creditCardLineSimilarityPercent > 80) {
-            $firstSpace = strpos($lines[$creditCardLineIndex], ' ');
-            $lastSpace = strrpos($lines[$creditCardLineIndex], ' ');
-            $this->receipt->total = substr($lines[$creditCardLineIndex], $firstSpace, $lastSpace-$firstSpace);
+            $firstSpace = strpos($this->lines[$creditCardLineIndex], ' ');
+            $lastSpace = strrpos($this->lines[$creditCardLineIndex], ' ');
+            $this->receipt->total = substr($this->lines[$creditCardLineIndex], $firstSpace, $lastSpace-$firstSpace);
         }
         // The id could be extracted from the line that starts with 'NYUGTASZAM:'. The id is followed by the ': '.
         // The date could be extracted from line after the id line. The date format is 'YYYY.MM.DD, HH:MM'
-        $idLineIndex = $this->getLineIndexWithLowestLevenshteinDistance($lines, self::ID_PATTERN, $firstLineAfterItems);
-        $this->receipt->basketId = trim(substr($lines[$idLineIndex], strpos($lines[$idLineIndex], ' ')+1));
-        $this->receipt->date = trim($lines[$idLineIndex+1]);
+        $idLineIndex = $this->getLineIndexWithLowestLevenshteinDistance(self::ID_PATTERN, $firstLineAfterItems);
+        $this->receipt->basketId = trim(substr($this->lines[$idLineIndex], strpos($this->lines[$idLineIndex], ' ')+1));
+        $this->receipt->date = trim($this->lines[$idLineIndex+1]);
     }
 
     // Returns the first line index that is not item.
@@ -85,6 +73,14 @@ class SparParserService extends AbstractParserService
         $numberOfLines = count($lines);
         for ($i = $from; $i < $numberOfLines; $i++) {
             if (!preg_match('/^[A-Z0-9]{1,3} /', $lines[$i])) {
+                // if the line is empty, then continue.
+                if (empty(trim($lines[$i]))) {
+                    continue;
+                }
+                // If the line contains the current total, then continue. The current total starts with 'RESZOSSZESEN'.
+                if (strpos($lines[$i], 'RESZOSSZESEN') !== false) {
+                    continue;
+                }
                 if ($itemParsing) {
                     return $i;
                 }
@@ -113,7 +109,10 @@ class SparParserService extends AbstractParserService
             }
             // handle the kg units.
             if (strpos(strtolower($lines[$i+1]), 'kg') !== false) {
-                $item['quantity'] = floatVal(str_replace(',', '.', trim(substr($lines[$i+1], 0, strpos($lines[$i+1], ' ')))));
+                $rawFloatNumber = str_replace(',', '.', trim(substr($lines[$i+1], 0, strpos($lines[$i+1], ' '))));
+                // remove the not numeric characters from the beginning of the string.
+                $rawFloatNumber = preg_replace('/^[^0-9]+/', '', $rawFloatNumber);
+                $item['quantity'] = floatVal($rawFloatNumber);
                 // if the quantity is 0, then it has to be updated to 1
                 // to prevent division by zero.
                 if ($item['quantity'] == 0) {
@@ -128,21 +127,52 @@ class SparParserService extends AbstractParserService
         }
     }
 
-    // Return the line index of the line that has the lowest levenshtein distance to the given pattern.
-    // If the pattern is not found, return -1.
-    private function getLineIndexWithLowestLevenshteinDistance($lines, $pattern, $startIndex = 0)
+    /*
+     * The company name is in the first line of the receipt.
+     * */
+    private function setupCompanyName()
     {
-        $minDistance = 999999;
-        $minDistanceIndex = -1;
-        $numberOfLines = count($lines);
-        for ($i = $startIndex; $i < $numberOfLines ; $i++) {
-            // Check only the first $pattern characters of the line.
-            $distance = levenshtein(substr($lines[$i], 0, strlen($pattern)), $pattern);
-            if ($distance < $minDistance) {
-                $minDistance = $distance;
-                $minDistanceIndex = $i;
-            }
+        $this->receipt->companyName = trim($this->lines[$this->readLineIndex]);
+        $this->readLineIndex++;
+    }
+    /*
+     * The company address is witten in the next 2 lines.
+     * */
+    private function setupCompanyAddress()
+    {
+        $this->receipt->companyAddress = trim($this->lines[$this->readLineIndex]);
+        $this->readLineIndex++;
+        $this->receipt->companyAddress .= ' ' . trim($this->lines[$this->readLineIndex]);
+        $this->readLineIndex++;
+    }
+
+    /*
+     * The market name is in one line.
+     * */
+    private function setupMarketName()
+    {
+        $this->receipt->marketName = trim($this->lines[$this->readLineIndex]);
+        $this->readLineIndex++;
+    }
+
+    /*
+     * The market address might be in one or more lines.
+     * */
+    private function setupMarketAddress($lineIndex)
+    {
+        for ($index = $this->readLineIndex; $index < $lineIndex; $index++) {
+            $this->receipt->marketAddress .= $this->lines[$index] . ' ';
+            $this->readLineIndex++;
         }
-        return $minDistanceIndex;
+        $this->receipt->marketAddress = trim($this->receipt->marketAddress);
+    }
+    /*
+     * The tax number is in one line. The actual number id after the 'Adószám: '.
+     * */
+    private function setupTaxNumber()
+    {
+        $taxNumberLine= $this->lines[$this->readLineIndex];
+        $this->receipt->taxNumber = trim(substr($taxNumberLine, strpos($taxNumberLine, ' ')+1));
+        $this->readLineIndex++;
     }
 }
