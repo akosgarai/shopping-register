3a4e651824f7d1a4f9293d6c75a0a53299d1b24a
akosgarai <szlem01@gmail.com>
2023-03-13 20:00:17 +0100

    Refactor spar parser

--- app/Services/Parser/SparParserService.php
+++ app/Services/Parser/SparParserService.php
@@ -35,25 +35,8 @@ class SparParserService extends AbstractParserService
         $this->setupMarketAddress($this->getLineIndexWithLowestLevenshteinDistance(self::TAX_NUMBER_PATTERN, $this->readLineIndex));
         // The tax number is followed by the 'ADÓSZÁM' pattern.
         $this->setupTaxNumber();
-        $firstLineAfterItems = $this->parseSparItemLines($this->lines, $this->readLineIndex);
-        // The total price could be extracted from the line that starts with 'BANKKARTYA' or 'ÖSSZESEN:'
-        $totalLineIndex = $this->getLineIndexWithLowestLevenshteinDistance(self::TOTAL_PATTERN, $firstLineAfterItems);
-        $creditCardLineIndex = $this->getLineIndexWithLowestLevenshteinDistance(self::CREDIT_CARD_PATTERN, $firstLineAfterItems);
-        // Check Total line. If the text similarity is higher than 0.8, then it is the total line.
-        $totalLineSimilarityPercent = 0;
-        similar_text(self::TOTAL_PATTERN, substr($this->lines[$totalLineIndex], 0, 8), $totalLineSimilarityPercent);
-        $creditCardLineSimilarityPercent = 0;
-        similar_text(self::CREDIT_CARD_PATTERN, substr($this->lines[$creditCardLineIndex], 0, 10), $creditCardLineSimilarityPercent);
-        // The total price is the last number in the line followed by ' Ft'
-        if ($totalLineSimilarityPercent > 80) {
-            $firstSpace = strpos($this->lines[$totalLineIndex], ' ');
-            $lastSpace = strrpos($this->lines[$totalLineIndex], ' ');
-            $this->receipt->total = substr($this->lines[$totalLineIndex], $firstSpace, $lastSpace-$firstSpace);
-        } else if ($creditCardLineSimilarityPercent > 80) {
-            $firstSpace = strpos($this->lines[$creditCardLineIndex], ' ');
-            $lastSpace = strrpos($this->lines[$creditCardLineIndex], ' ');
-            $this->receipt->total = substr($this->lines[$creditCardLineIndex], $firstSpace, $lastSpace-$firstSpace);
-        }
+        $firstLineAfterItems = $this->parseSparItemLines();
+        $this->setupTotalPrice($firstLineAfterItems);
         // The id could be extracted from the line that starts with 'NYUGTASZAM:'. The id is followed by the ': '.
         // The date could be extracted from line after the id line. The date format is 'YYYY.MM.DD, HH:MM'
         $idLineIndex = $this->getLineIndexWithLowestLevenshteinDistance(self::ID_PATTERN, $firstLineAfterItems);
@@ -62,7 +45,7 @@ class SparParserService extends AbstractParserService
     }
 
     // Returns the first line index that is not item.
-    private function parseSparItemLines($lines, $from)
+    private function parseSparItemLines()
     {
         // The lines containing the items are following the next pattern:
         // a prefix (3 character) followed by a space. The next characters are the name of the item, followed by a space and the price.
@@ -70,15 +53,13 @@ class SparParserService extends AbstractParserService
         // The items are finished, when the itemParsing flag is true and the next line does not match the pattern.
         $itemParsing = false;
         $item = [];
-        $numberOfLines = count($lines);
-        for ($i = $from; $i < $numberOfLines; $i++) {
-            if (!preg_match('/^[A-Z0-9]{1,3} /', $lines[$i])) {
-                // if the line is empty, then continue.
-                if (empty(trim($lines[$i]))) {
-                    continue;
-                }
-                // If the line contains the current total, then continue. The current total starts with 'RESZOSSZESEN'.
-                if (strpos($lines[$i], 'RESZOSSZESEN') !== false) {
+        $numberOfLines = count($this->lines);
+        for ($i = $this->readLineIndex; $i < $numberOfLines; $i++) {
+            $currentLine = $this->lines[$i];
+            // increment the read line index.
+            $this->readLineIndex++;
+            if (!preg_match('/^[A-Z0-9]{1,3} /', $currentLine)) {
+                if ($this->notRelevantLine($currentLine)) {
                     continue;
                 }
                 if ($itemParsing) {
@@ -87,29 +68,33 @@ class SparParserService extends AbstractParserService
                 continue;
             }
             $itemParsing = true;
-            $lastSpaceIndex = strrpos($lines[$i], ' ');
-            $priceCandidate = trim(substr($lines[$i], $lastSpaceIndex+1));
-            // replace every not digit character with empty string
-            $priceCandidate = preg_replace('/[^0-9]/', '', $priceCandidate);
+            $lastSpaceIndex = strrpos($currentLine, ' ');
+            $priceCandidateRaw = trim(substr($currentLine, $lastSpaceIndex+1));
+            // replace every not digit character with empty string at the beginning of the string.
+            $priceCandidate = preg_replace('/[^0-9]/', '', $priceCandidateRaw);
+            // replace every not digit character with '0' string at the end of the string.
+            //$priceCandidate = preg_replace('/[^0-9]+$/', '0', $priceCandidate);
             $item['quantity'] = 1;
             $item['quantity_unit_id'] = 3;
 
             if (is_numeric($priceCandidate)) {
                 $item['price'] = $priceCandidate;
-                $item['name'] = trim(substr($lines[$i], 4, $lastSpaceIndex-4));
+                $item['name'] = trim(substr($currentLine, 4, $lastSpaceIndex-4));
                 $item['unit_price'] = floatVal($item['price']) / $item['quantity'];
                 $this->receipt->items[] = $item;
                 continue;
             }
 
-            $item['name'] = trim(substr($lines[$i], 4));
+            $item['name'] = trim(substr($currentLine, 4));
             // find the next not empty line
-            while (empty(trim($lines[$i+1]))) {
+            while (empty(trim($this->lines[$i + 1]))) {
                 $i++;
+                $this->readLineIndex++;
             }
+            $currentLine = $this->lines[$i + 1];
             // handle the kg units.
-            if (strpos(strtolower($lines[$i+1]), 'kg') !== false) {
-                $rawFloatNumber = str_replace(',', '.', trim(substr($lines[$i+1], 0, strpos($lines[$i+1], ' '))));
+            if (strpos(strtolower($currentLine), 'kg') !== false) {
+                $rawFloatNumber = str_replace(',', '.', trim(substr($currentLine, 0, strpos($currentLine, ' '))));
                 // remove the not numeric characters from the beginning of the string.
                 $rawFloatNumber = preg_replace('/^[^0-9]+/', '', $rawFloatNumber);
                 $item['quantity'] = floatVal($rawFloatNumber);
@@ -120,7 +105,7 @@ class SparParserService extends AbstractParserService
                 }
                 $item['quantity_unit_id'] = 1;
             }
-            $item['price'] = trim(substr($lines[$i+1], strrpos($lines[$i+1], ' ')+1));
+            $item['price'] = trim(substr($currentLine, strrpos($currentLine, ' ')+1));
             $item['unit_price'] = floatVal($item['price']) / $item['quantity'];
             $this->receipt->items[] = $item;
             $i++;
@@ -187,5 +172,49 @@ class SparParserService extends AbstractParserService
         $taxNumberLine= $this->lines[$this->readLineIndex];
         $this->receipt->taxNumber = trim(substr($taxNumberLine, strpos($taxNumberLine, ' ')+1));
         $this->readLineIndex++;
+    }
+
+    /*
+     * The total price could be extracted from the line starts with 'Osszesen', or
+     * from the line starts with the credit card pattern.
+     * */
+    private function setupTotalPrice($firstLineAfterItems)
+    {
+        // The total price could be extracted from the line that starts with 'BANKKARTYA' or 'ÖSSZESEN:'
+        $totalLineIndex = $this->getLineIndexWithLowestLevenshteinDistance(self::TOTAL_PATTERN, $firstLineAfterItems);
+        $creditCardLineIndex = $this->getLineIndexWithLowestLevenshteinDistance(self::CREDIT_CARD_PATTERN, $firstLineAfterItems);
+        // Check Total line. If the text similarity is higher than 0.8, then it is the total line.
+        $totalLineSimilarityPercent = 0;
+        similar_text(self::TOTAL_PATTERN, substr($this->lines[$totalLineIndex], 0, 8), $totalLineSimilarityPercent);
+        $creditCardLineSimilarityPercent = 0;
+        similar_text(self::CREDIT_CARD_PATTERN, substr($this->lines[$creditCardLineIndex], 0, 10), $creditCardLineSimilarityPercent);
+        // The total price is the last number in the line followed by ' Ft'
+        if ($totalLineSimilarityPercent > 80) {
+            $firstSpace = strpos($this->lines[$totalLineIndex], ' ');
+            $lastSpace = strrpos($this->lines[$totalLineIndex], ' ');
+            $this->receipt->total = substr($this->lines[$totalLineIndex], $firstSpace, $lastSpace-$firstSpace);
+        } else if ($creditCardLineSimilarityPercent > 80) {
+            $firstSpace = strpos($this->lines[$creditCardLineIndex], ' ');
+            $lastSpace = strrpos($this->lines[$creditCardLineIndex], ' ');
+            $this->receipt->total = substr($this->lines[$creditCardLineIndex], $firstSpace, $lastSpace-$firstSpace);
+        }
+    }
+
+    /*
+     * Checks a line for the given patterns. If a pattern is found, then it returns true.
+     * */
+    private function notRelevantLine($lineRaw)
+    {
+        // if the line is empty, then continue.
+        if (empty(trim($lineRaw))) {
+            return true;
+        }
+        $patterns = ['AFA', 'ÁFA', 'RESZOSSZESEN', 'Megnevez'];
+        foreach ($patterns as $pattern) {
+            if (strpos($lineRaw, $pattern) !== false) {
+                return true;
+            }
+        }
+        return false;
     }
 }
