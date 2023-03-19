f9d4d072798f8a33e389191c9e3c0c29787fef23
akosgarai <szlem01@gmail.com>
2023-03-19 13:05:23 +0100

    Delete Filepicker as it is only used on the original scan page

--- resources/views/livewire/component/filepicker.blade.php
+++
@@ -1,4 +0,0 @@
-<div class="input-group mb-3">
-    <input type="file" class="form-control" wire:model="file">
-    <button class="btn btn-outline-secondary" type="button" @if(!$file) disabled @endif wire:click="broadcastImage">{{ __('Upload') }}</button>
-</div>
